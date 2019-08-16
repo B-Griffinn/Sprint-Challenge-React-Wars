492f573ecba7ddad20840d7e9e4522048a7256aa
Ben <bigriffin94@outlook.com>
2019-08-16 10:55:00 -0700

    Done done done.

--- starwars/src/components/NextComponents/Next-Button.jsx
+++ starwars/src/components/NextComponents/Next-Button.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import Pagination from "pagination";
 
 const NextButton = (props) => {
-
-
-    
-
+  
     return (
         <div className="btn-wrapper">
-        <button>Next Page!</button>
+        <button className="pagination">Next Page!</button>
             {/* <button className="btn">Click Me!</button> */}
         </div>
     )
